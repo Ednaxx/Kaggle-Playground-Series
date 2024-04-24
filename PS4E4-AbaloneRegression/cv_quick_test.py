4b8a8900cef1f467429ee3a5a702a83fc2ed267f
Alexandre Morais <xandre2setembro2002@hotmail.com>
2024-04-24 13:34:44 -0300

    PS4E4 final submission

--- PS4E4-AbaloneRegression/cv_quick_test.py
+++ PS4E4-AbaloneRegression/cv_quick_test.py
@@ -10,6 +10,6 @@ def cv_quick_test(X, y):
     regressor = XGBRegressor(n_estimators=100, random_state=42, n_jobs=-1)
 
     regressor.fit(X_train, y_train)
-    score = cross_val_score(regressor, X, y, scoring="neg_mean_squared_log_error", cv=5).mean()
+    score = cross_val_score(regressor, X, y, scoring="neg_mean_squared_log_error", cv=10).mean()
     score = np.sqrt(-score)
     return score
